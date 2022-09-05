b311665a3d20ebdc8f51c955fad83a7bffdbbb7a
Brandon Vidal <brandon.vidal@ceiba.com.com>
2022-09-05 10:03:51 -0500

    GRAL - Material Icons

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     CommonModule,
+    MatIconModule
   ],
   exports: [
     NavbarComponent,
